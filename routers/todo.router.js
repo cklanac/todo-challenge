4f4d8ae9edbdad84e864d0f045626ecd26338c36
Chris Klanac <cklanac@users.noreply.github.com>
2017-12-25 16:44:13 -0600

    add promises solution

--- routers/todo.router.js
+++ routers/todo.router.js
@@ -13,6 +13,98 @@ simDB.initialize(data).then(data => todos = data);
 /**
  * ADD ENDPOINTS HERE
  */
+router.get('/', (req, res, next) => {
+
+  // Using promises
+  todos.find({})
+    .then(list => res.json(list))
+    .catch(next); // error handler
+
+});
+
+router.post('/', (req, res, next) => {
+  const { title } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err); // error handler
+  }
+  const newItem = { title };
+  newItem.completed = false;
+  
+  // Using promises
+  todos.create(newItem)
+    .then(item => res.location(`/items/${item.id}`).status(201).json(item))
+    .catch(next); // error handler
+
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  // Using promises
+  todos.findById(id)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next(); // 404 handler
+      }
+    })
+    .catch(next); // error handler
+
+});
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const replaceItem = {};
+  const updateableFields = ['title', 'completed'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      replaceItem[field] = req.body[field];
+    }
+  });
+
+  /***** Never trust users - validate input *****/
+  if (!replaceItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err); // error handler
+  }
+
+  // Using promises
+  todos.findByIdAndUpdate(id, replaceItem)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next(); // 404 handler
+      }
+    })
+    .catch(next); // error handler
+
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  // Using promises
+  todos.findByIdAndRemove(id)
+    .then(count => {
+      if (count) {
+        res.status(204).end();
+      } else {
+        next(); // 404 handler
+      }
+    })
+    .catch(next); // error handler
+    
+});
 
 
 module.exports = router;
